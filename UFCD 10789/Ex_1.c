25456f05667e4d1ce9a8a439641dd02a940a08a8
Thais Silva <Thais.Silva.T0126462@edu.atec.pt>
2025-02-20 01:57:07 -0800

    Elaboração do ex_1

--- UFCD 10789/Ex_1.c
+++ UFCD 10789/Ex_1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world");
+    int Num=0;
+    for(int i=0; i<10; i++){
+        Num = Num+2;
+        printf("Num = %d, i = %d\n", Num, i);
+        printf("aquiFOR\n\n");
+    }
+    //printf("Hello world");
     return 0;
 }
