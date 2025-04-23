d174af7d703fda42e58300da39bf7354aa19fe72
Thais Silva <Thais.silva.t0126462@edu.atec.pt>
2025-04-23 10:05:55 +0100

    ExFor

--- UFCD 10789/C/ExFor/ExFor.c
+++ UFCD 10789/C/ExFor/ExFor.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    // Ler 10 números
+    int num = 0;
+
+    // Determinar o número par e numero impar…
+    for (int n = 0; n < 10; n++){
+        printf("Insira o %dº número: ", n + 1);
+        scanf("%d", &num);
+
+        if (num % 2 == 0){
+            puts("Par\n");
+        }
+        else{
+            puts("Ímpar\n");
+        }
+    }
+}
